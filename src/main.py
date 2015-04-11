6a3cc37688ef3d72ea3ffda038f69e9487c5c61c
Andreas A. Flakstad <andflak@gmail.com>
2015-04-11 11:05:18 +0200

    Speech recognition implemented.

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@
 __author__ = 'andreas'
 
 import threading
+import pyaudio
 
 from pocketsphinx import *
 from sphinxbase import *
 
-hmm = 'cmusphinx.-en-us-5.2/'
+hmm = 'cmusphinx-en-us-5.2/'
 lm = 'TAR2860/2860.lm'
 dic = 'TAR2860/2860.dic'
 
@@ -25,7 +26,64 @@ class PocketSphinx(threading.Thread):
         config.set_string('-dict', dic)
         return config
 
+    def run(self):
+        print("Pocketsphinx running..")
+        self.running = True
+        decoder = Decoder(self.configure())
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+        stream.start_stream()
+        speaking = True
+        decoder.start_utt()
 
+        while self.running:
+            buf = stream.read(1024)
+            if buf:
+                decoder.process_raw(buf, False, False)
+                try:
+                    # If partial results:
+                    if decoder.hyp().hypstr != '':
+                        hypstr = decoder.hyp().hypstr
+                        print('Partial decoding result: ' + hypstr)
+                except AttributeError:
+                    pass
+                # If the speech has ended:
+                if decoder.get_in_speech() != speaking:
+                    speaking = decoder.get_in_speech()
+                    # If the speech has ended:
+                    if not speaking:
+                        decoder.end_utt()
+                        try:
+                            if decoder.hyp().hypstr != '':
+                                decoded_string = decoder.hyp().hypstr
+                                print('Stream decoding result: ' + decoded_string)
+                        except AttributeError:
+                            pass
+                        decoder.start_utt() # Tell the decoder to prepare for a new sequence of words.
+                        #print('Stopped listening.')
+                else:
+                    #print('Started listening..')
+                    pass
+            else:
+                break
+        decoder.end_utt()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    def stop(self):
+        self.running = False
+
+def run_pocketsphinx():
+    try:
+        print("Starting script..")
+        ps = PocketSphinx()
+        ps.start()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        print('Keyboard interrupt.')
+        ps.stop()
 
 if __name__ == '__main__':
-    print("Starting script..")
+    run_pocketsphinx()
--- src/subprocess-pocketsphinx.py
+++ src/subprocess-pocketsphinx.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
 import subprocess
 
+# Reaches out to the terminal to run continuous speech recognition with a provide
+# language model and dictionary.
 subprocess.call(["pocketsphinx_continuous", "-inmic", "yes", "-lm", "TAR2860/2860.lm", "-dict", "TAR2860/2860.dic"])
